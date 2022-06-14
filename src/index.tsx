2ecfb4cc19d33ed082997ba7e348749b1a1bfc64
dLeczycki <danielleczycki@gmail.com>
2022-06-14 19:17:41 +0200

    Add ChakraUI to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import './index.css';
 import App from './App';
 
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
 );
