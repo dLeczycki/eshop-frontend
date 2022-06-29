03cb4075f07d8cc42fb62dfe864d795b6a081c4c
dLeczycki <danielleczycki@gmail.com>
2022-06-29 09:08:01 +0200

    Upgrade ProductCard to Product navigation

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,7 +1,6 @@
-import { Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Image, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
 import { config } from "../../config/config";
 import { CheckoutProduct, Product } from "types";
-import { useNavigate } from "react-router-dom";
 import { useCheckout } from "../../contexts/checkout/checkout.context";
 import { SyntheticEvent } from "react";
 
@@ -11,12 +10,7 @@ interface Props{
 
 export const ProductCard = (props: Props) => {
   const {product} = props;
-  const navigate = useNavigate();
   const checkout = useCheckout();
-
-  const handleRedirectToProduct = () => {
-    navigate(`/produkty/${product.id}`);
-  }
 
   const handleAddToCheckout = (e: SyntheticEvent) => {
     e.stopPropagation();
@@ -26,55 +20,73 @@ export const ProductCard = (props: Props) => {
   }
 
   return (
-    <Flex
+    <LinkBox 
+      as="div"
       mx={4}
       my={4}
-      direction="column"
-      justify="space-between"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
       boxShadow="2px 2px 5px 0 rgba(0,0,0,0.2)"
       borderRadius="10px"
       cursor="pointer"
-      onClick={handleRedirectToProduct}
     >
-      <Image 
-        src={product.images && `${config.api}/${product.images[0].url}`} 
-        alignSelf="center"
-        w="240px"
-        h="200px"
-        objectFit="contain"
-      />
-      <Flex
-        px={4}
-        justify="space-between"
-        align="center"
+      <LinkOverlay 
+        href={`/produkty/${product.id}`} 
+        flexGrow={1} 
+        transitionDuration=".2s"
+        _hover={{
+          outline: "none", 
+          boxShadow: "0px 0px 5px 5px rgba(49, 130, 206, 0.2)",
+          borderTopRadius: "10px",
+        }}
+        _focus={{
+            outline: "none", 
+            boxShadow: "0px 0px 5px 5px rgba(49, 130, 206, 0.2)",
+            borderTopRadius: "10px",
+          }}
       >
-        <Heading 
-          as="h3" 
-          size="sm"
-          py={2} 
-          textAlign="center" 
-          color="blue.600"
-          maxWidth="50%"
+        <Image 
+          src={product.images && `${config.api}/${product.images[0].url}`} 
+          alignSelf="center"
+          w="240px"
+          h="200px"
+          objectFit="contain"
+        />
+        <Flex
+          px={4}
+          justify="space-between"
+          align="center"
         >
-          {product.name}
-        </Heading>
-        <Text
-          textAlign="center"
-          py={2}
-          maxW="100px"
-          color="gray.800"
-          fontWeight="bold"
-        >
-          {product.price}zł
-        </Text>
-      </Flex>
+          <Heading 
+            as="h3" 
+            size="sm"
+            py={2} 
+            textAlign="center" 
+            color="blue.600"
+            maxWidth="50%"
+          >
+            {product.name}
+          </Heading>
+          <Text
+            textAlign="center"
+            py={2}
+            maxW="100px"
+            color="gray.800"
+            fontWeight="bold"
+          >
+            {product.price}zł
+          </Text>
+        </Flex>
+      </LinkOverlay>
       <Button
         colorScheme="blue"
         borderTopRadius="unset"
+        w="100%"
         onClick = {(e) => handleAddToCheckout(e)}
       >
         Dodaj do koszyka
       </Button>
-    </Flex>
+    </LinkBox>
   )
 }
