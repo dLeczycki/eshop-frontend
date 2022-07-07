423b44cb1fe82ed037bccd3090ed650c8101400f
dLeczycki <danielleczycki@gmail.com>
2022-07-07 20:13:18 +0200

    Add empty ProductDetails route

--- src/components/router/Router.tsx
+++ src/components/router/Router.tsx
@@ -6,12 +6,14 @@ import { Order } from "../order/Order"
 import { Summary } from "../order/summary/Summary"
 import { PaymentTemporaryPage } from "../order/payment/PaymentTemporaryPage"
 import { OrderFinished } from "../order/finished/OrderFinished"
+import { ProductDetails } from "../products/ProductDetails"
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/produkty" element={<Products />} />
+      <Route path="/produkty/:id" element={<ProductDetails />} />
       <Route path="/zamowienie/podsumowanie" element={<Summary />} />
       <Route path="/zamowienie/platnosc" element={<PaymentTemporaryPage />} />
       <Route path="/zamowienie/zakonczone" element={<OrderFinished />} />
